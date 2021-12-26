0ba6505f211b4f9708d614f50d4c3a267b5a9800
Sunji <sophieadenn@gmail.com>
2021-12-26 17:08:20 +0900

    사이즈 바

---
+++ src/components/character-size-bar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CharacterSizeBar = () => {
+  // TODO: size bar는 스크롤 바로 구현해보자
+  return (
+    <div>
+      <h2>Size bar</h2>
+      <p>sizebar is here!</p>
+    </div>
+  );
+};
